8a39612e1cefd77bca1dc2cd590540f3a3d7b2d9
taka-yoshio <takanari_yoshio@e-psilon.co.jp>
2024-10-25 22:29:52 +0900

    fix: 修正内容3

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [2, 'always', 72], // ヘッダーの最大長を72文字に制限
-    'type-empty': [2, 'never'], // タイプは空にできない
-    'subject-empty': [2, 'never'], // サブジェクトは空にできない
+    "header-max-length": [2, "always", 72], // ヘッダーの最大長を72文字に制限
+    "type-empty": [2, "never"], // タイプは空にできない
+    "subject-empty": [2, "never"], // サブジェクトは空にできない
     // 他のルールをここに追加
   },
 };
